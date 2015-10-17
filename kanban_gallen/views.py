c2fee7d5cfb0952f29db3ca2bd9f9f4fa6cbe0c9
Gallen <seirios0107@gmail.com>
2015-10-18 01:16:17 +0900

    [backend] create the new card and add to db

--- kanban_gallen/views.py
+++ kanban_gallen/views.py
@@ -8,11 +8,16 @@ from flask import abort, render_template
 from sqlalchemy.exc import IntegrityError
 
 from kanban_gallen import app, db
-from .models import KanbanColumn
+from .models import KanbanColumn, KanbanPortlet
 
 
 def create_column_result(success, id=None, title=None):
   return json.dumps({'success': success, 'id': id, 'title': title})
+
+
+def create_portlet_result(success, id=None, title=None, content=None):
+  return json.dumps({'success': success, 'id': id, 'title': title,
+                    'content': content})
 
 
 @app.route('/', methods=['GET'])
@@ -34,3 +39,25 @@ def create_column():
     abort(httplib.BAD_REQUEST, 'BAD REQUEST')
     return create_column_result(False)
   return create_column_result(True, column.id, column.title)
+
+
+@app.route('/create/portlet/<column_id>', methods=['POST'])
+@app.route('/create/portlet/<column_id>/', methods=['POST'])
+def create_portlet(column_id):
+  column = KanbanColumn.query.get(column_id)
+
+  if not column:
+    abort(httplib.NOT_FOUND, 'COLUMN NOT FOUND')
+
+  new_title = "New Portlet"
+  new_content = "Lorem ipsum"
+  portlet = KanbanPortlet(new_title, new_content)
+  try:
+    column.portlets.append(portlet)
+    db.session.add(portlet)
+    db.session.commit()
+  except IntegrityError:
+    abort(httplib.BAD_REQUEST, 'BAD REQUEST')
+    return create_portlet_result(False)
+  return create_portlet_result(True, portlet.id, portlet.title,
+                               portlet.content)
