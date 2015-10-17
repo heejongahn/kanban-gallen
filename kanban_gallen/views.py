96372d10bc5dff92b8ff5936f080404afecd2f14
nellaG <nellaG@github.com>
2015-10-17 23:46:10 +0900

    [backend] 'Create Column' button makes the new column and add to db

--- kanban_gallen/views.py
+++ kanban_gallen/views.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # vim: fileencoding=utf-8 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
 
-from flask import render_template
+import httplib
+import json
 
-from kanban_gallen import app
+from flask import abort, render_template
+from sqlalchemy.exc import IntegrityError
+
+from kanban_gallen import app, db
 from .models import KanbanColumn
 
 
@@ -11,3 +15,16 @@ from .models import KanbanColumn
 def index():
   columns = KanbanColumn.query.all()
   return render_template('index.html', columns=columns)
+
+
+@app.route('/create/column', methods=['POST'])
+@app.route('/create/column/', methods=['POST'])
+def create_column():
+  column = KanbanColumn('New Column')
+  try:
+    db.session.add(column)
+    db.session.commit()
+  except IntegrityError:
+    abort(httplib.BAD_REQUEST, 'BAD REQUEST')
+    return json.dumps({'success': False, 'id': None})
+  return json.dumps({'success': True, 'id': column.id})
