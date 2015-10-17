3c254d9c69afff6bb73fedf6a864f578ae3b761b
nellag <nellag@github.com>
2015-10-17 11:16:16 +0900

    [ui] initialize index page

--- kanban_gallen/views.py
+++ kanban_gallen/views.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # vim: fileencoding=utf-8 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+
+from flask import render_template
+
+from kanban_gallen import app
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
